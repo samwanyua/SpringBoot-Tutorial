a9db9a00357f624d3034a0d25f3e71a4430c38f1
samwanyua <samexample8@gmail.com>
2024-05-06 19:46:07 +0300

    Building API with SpringBoot

--- src/main/java/com/example/sam/backend/BackendApplication.java
+++ src/main/java/com/example/sam/backend/BackendApplication.java
@@ -1,13 +1,36 @@
 package com.example.sam.backend;
 
+import com.example.sam.backend.students.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
+@RestController // make the class to serve rest endpoints
 public class BackendApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@GetMapping
+	public List<Student> hello(){
+		return List.of(
+				new Student(
+						1L,
+						"Mariam",
+						"mariam.jamal@gmail.com",
+						LocalDate.of(2000, Month.JANUARY, 5),
+						21
+
+				)
+		);
 	}
 
 }
