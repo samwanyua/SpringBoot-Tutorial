fe18e56b68dbc83857f5b34d69426493a68c6853
samwanyua <samexample8@gmail.com>
2024-05-28 23:42:01 +0300

    Initial commit

--- src/main/java/com/example/sam/fitness/Fitness.java
+++ src/main/java/com/example/sam/fitness/Fitness.java
@@ -1,4 +1,4 @@
-package com.example.sam.backend;
+package com.example.sam.fitness;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController // make the class to serve rest endpoints
-public class BackendApplication {
+public class Fitness {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(Fitness.class, args);
 	}
 
 }
--- src/test/java/com/example/sam/fitness/BackendApplicationTests.java
+++ src/test/java/com/example/sam/fitness/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.sam.backend;
+package com.example.sam.fitness;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
